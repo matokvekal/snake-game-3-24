d221b4497a6ddc4279a21f9d313f399d260f883f
Laptop Admin <mictavim@gmail.com>
2024-03-03 21:00:20 +0200

    start build game function

--- snake.js
+++ snake.js
@@ -1,20 +1,78 @@
-window.onload = function() {
-    //add events listeners
-    //get the canvas
-    //interval game
+window.onload = function () {
+  document.addEventListener("keyup", keyPush);
+  const canv = document.getElementById("game");
+  const ctx = canv.getContext("2d");
+  setInterval(game, 1000 / 2);
+};
+
+let snakeheadX = 1;
+let snakeheadY = 1;
+let gridSize = 20;
+let foodX = 7;
+let foodY = 2;
+let moveX = 0;
+let moveY = 0;
+let snakeBody = [{x:1,y:1}];
+let snakeLength = 1;
+let score = 0;
+
+function game() {
+  document.getElementById("score").innerHTML = score;
+  snakeheadX += moveX;
+  snakeheadY += moveY;
+
+  ctx.fillStyle = "blue";
+  ctx.fillRect(0, 0, canv.width, canv.height);
+
+  for (let i = 0; i < snakeBody.length; i++) {
+    ctx.fillStyle = "white";
+    ctx.fillRect(
+      snakeBody[i].x * 1,
+      snakeBody[i].y * 1,
+      gridSize - 2,
+      gridSize - 2
+    );
+    if(snakeBody[i].x===snakeheadX && snakeBody[i].y===snakeheadY){
+        snakeLength=1;
+        score=0;
+    }
+    snakeBody.push({x:snakeheadX,y:snakeheadY});
+  }
 }
+
 ////////NEW GAME//////
 ////////NEW GAME//////
 //arguments - config object
 
 //function game
-    //mange the score
-    //mange key press/movment
-    //manage snake length
-    //eating food(grow,points,new random food)
-    //borders
-    //collisions
-    //game over
-    //
+//mange the score
+//mange key press/movment
+//manage snake length
+//eating food(grow,points,new random food)
+//borders
+//collisions
+//game over
+//
 
 //function keyPush listener
+
+function keyPush(evt) {
+  switch (evt.keyCode) {
+    case 37:
+      moveX = -1;
+      moveY = 0;
+      break;
+    case 38:
+      moveX = 0;
+      moveY = -1;
+      break;
+    case 39:
+      moveX = 1;
+      moveY = 0;
+      break;
+    case 40:
+      moveX = 0;
+      moveY = 1;
+      break;
+  }
+}
