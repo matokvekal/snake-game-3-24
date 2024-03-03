1ce452f5acf1f88153b8d9b5af2e969151ae9516
Laptop Admin <mictavim@gmail.com>
2024-03-03 21:18:52 +0200

    start build game function

--- snake.js
+++ snake.js
@@ -1,7 +1,9 @@
+//we start the game after all  html and javscript is loaded
 window.onload = function () {
   document.addEventListener("keyup", keyPush);
   canv = document.getElementById("game");
   ctx = canv.getContext("2d");
+  //here we run the game function every 1000/2 milliseconds
   setInterval(game, 1000 / 2);
 };
 //configurations
@@ -16,15 +18,16 @@ let snakeBody = [{ x: 1, y: 1 }];
 let snakeLength = 1;
 let score = 0;
 
-
+//the main game function
 function game() {
   document.getElementById("score").innerHTML = score;
   snakeheadX += moveX;
   snakeheadY += moveY;
 debugger
+//here we clean the canvas from any previous drawings
   ctx.fillStyle = "blue";
   ctx.fillRect(0, 0, canv.width, canv.height);
-
+//here  we drow all parts of the snake  
   for (let i = 0; i < snakeBody.length; i++) {
     ctx.fillStyle = "white";
     ctx.fillRect(
@@ -33,13 +36,14 @@ debugger
       gridSize - 2,
       gridSize - 2
     );
-    //the snamke hit himself
+    //here we check if the snake head hit the snake body
     if (snakeBody[i].x === snakeheadX && snakeBody[i].y === snakeheadY) {
       snakeLength = 1;
       score = 0;
     }
-    //push the head into the array
+    //push the new head pusition into the array
     snakeBody.push({ x: snakeheadX, y: snakeheadY });
+    // here we remove the tail of the snake
     while (snakeBody.length > snakeLength) {
       snakeBody.shift();
     }
