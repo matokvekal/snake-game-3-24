cfcdfa99db6f1a3377bf47df59bd751702e40282
Laptop Admin <mictavim@gmail.com>
2024-03-06 19:43:33 +0200

    finish snake game

--- snake.js
+++ snake.js
@@ -1,33 +1,49 @@
 //we start the game after all  html and javscript is loaded
 window.onload = function () {
-  document.addEventListener("keyup", keyPush);
+  document.addEventListener("keydown", keyPush);
   canv = document.getElementById("game");
   ctx = canv.getContext("2d");
   //here we run the game function every 1000/2 milliseconds
-  setInterval(game, 1000 / 2);
+  setInterval(game, 1000 / 3);
 };
 //configurations
-let snakeheadX = 1;
-let snakeheadY = 1;
+let canv;
+let ctx;
+let snakeheadX = 0;
+let snakeheadY =0;
 let gridSize = 20;
 let foodX = 7;
 let foodY = 2;
 let moveX = 0;
 let moveY = 0;
-let snakeBody = [{ x: 1, y: 1 }];
+let snakeBody = [];
 let snakeLength = 1;
 let score = 0;
 
 //the main game function
 function game() {
+  debugger
   document.getElementById("score").innerHTML = score;
-  snakeheadX += moveX;
-  snakeheadY += moveY;
-debugger
-//here we clean the canvas from any previous drawings
+  snakeheadX += moveX * gridSize;
+  snakeheadY += moveY * gridSize;
+ 
+  //here we check if the snamke head hit the borders
+  if (snakeheadX < 0) {
+    snakeheadX = canv.width - gridSize;
+  }
+  if (snakeheadX > canv.width - gridSize) {
+    snakeheadX = 0;
+  }
+  if (snakeheadY < 0) {
+    snakeheadY = canv.height - gridSize;
+  }
+  if (snakeheadY > canv.height - gridSize) {
+    snakeheadY = 0;
+  }
+  //here we clean the canvas from any previous drawings
   ctx.fillStyle = "blue";
   ctx.fillRect(0, 0, canv.width, canv.height);
-//here  we drow all parts of the snake  
+  //here  we drow all parts of the snake
   for (let i = 0; i < snakeBody.length; i++) {
     ctx.fillStyle = "white";
     ctx.fillRect(
@@ -41,13 +57,29 @@ debugger
       snakeLength = 1;
       score = 0;
     }
-    //push the new head pusition into the array
-    snakeBody.push({ x: snakeheadX, y: snakeheadY });
-    // here we remove the tail of the snake
-    while (snakeBody.length > snakeLength) {
-      snakeBody.shift();
-    }
   }
+  //push the new head pusition into the array
+  snakeBody.push({ x: snakeheadX, y: snakeheadY });
+  // here we remove the tail of the snake
+  while (snakeBody.length > snakeLength) {
+    snakeBody.shift();
+  }
+
+  //here we check if the snake head hit the food
+  debugger;
+  if (foodX* gridSize === snakeheadX && foodY* gridSize === snakeheadY) {
+    debugger
+    snakeLength++;
+    score += 20;
+    foodX = Math.floor(Math.random() * (canv.width / gridSize));
+    foodY = Math.floor(Math.random() * (canv.height / gridSize));
+  }
+  ctx.fillStyle = "red";
+  ctx.fillRect(
+    foodX * gridSize, 
+    foodY * gridSize, 
+    gridSize - 2, 
+    gridSize - 2);
 }
 
 ////////NEW GAME//////
